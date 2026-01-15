2de8a8ff09aa9dc06021b73621bfa4070fab0dfd
rufeii <helinfeng_123@qq.com>
2026-01-15 14:10:34 +0800

    Add files via upload

---
+++ 数组/一维数组/demo1.java
@@ -0,0 +1,23 @@
+package rufeii.code;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(getmax(arr));
+    }
+    public static int getmax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
