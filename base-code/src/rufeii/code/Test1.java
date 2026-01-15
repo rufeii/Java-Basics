beed12f92ffcefa15952cf5d475aabf9d4c88912
rufeii <helinfeng_123@qq.com>
2026-01-15 13:59:33 +0800

    Delete base-code/src/rufeii/code directory

--- base-code/src/rufeii/code/Test1.java
+++
@@ -1,9 +0,0 @@
-package rufeii.code;
-import java.util.Scanner;//导入这个类
-
-//java实现键盘录入
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.println(8888);
-    }
-}
